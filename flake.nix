ac03f4e812b1f5ec73c0d9121f7733a2c10293db
haruki7049 <tontonkirikiri@gmail.com>
2024-08-29 17:40:43 +0900

    feat(zenn-cli): Init

--- flake.nix
+++ flake.nix
@@ -12,8 +12,38 @@
           inherit system;
         };
         treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        nodeEnv = pkgs.callPackage "${nixpkgs}/pkgs/development/node-packages/node-env.nix" {
+          inherit (pkgs)
+            stdenv
+            lib
+            python2
+            runCommand
+            writeTextFile
+            writeShellScript
+            ;
+          nodejs = pkgs.nodejs_20;
+          libtool = null;
+        };
+        zenn-cli = nodeEnv.buildNodePackage rec {
+          name = "zenn";
+          packageName = "zenn-cli";
+          version = "0.1.155";
+          src = pkgs.fetchurl {
+            url = "https://registry.npmjs.com/zenn-cli/-/zenn-cli-${version}.tgz";
+            hash = "sha256-b3YTRoSA3mRq3njVcWn+vlvjSS/98rWWIOLNlstTREg=";
+          };
+
+          production = true;
+          bypassCache = true;
+          reconstructLock = true;
+        };
       in
       {
+        packages = {
+          inherit zenn-cli;
+          default = zenn-cli;
+        };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             # Zenn-cli
