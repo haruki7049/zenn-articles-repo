65edce887b25513455a4fc627a7b3380544e5e2f
haruki7049 <tontonkirikiri@gmail.com>
2024-08-29 17:25:17 +0900

    style(treefmt-nix): Init

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -25,6 +27,15 @@
           shellHook = ''
             export PS1="\n[nix-shell:\w]$ "
           '';
+        };
+
+        # Use `nix fmt`
+        formatter =
+          treefmtEval.config.build.wrapper;
+
+        # Use `nix flake check`
+        checks = {
+          formatting = treefmtEval.config.build.check self;
         };
       });
 }
